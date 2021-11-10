79eb16b0bffeb20d72dcf0f2088d97b964cbec3c
Andrei Fiodorov <a.fiodorov@noirix.com>
2021-11-10 22:24:18 +0100

    fetch characters

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'antd';
+import axios from '../utils/axios';
 import Character from './Character';
 
 import styles from './Characters.module.scss';
 
+interface Pagination {
+    current: number;
+    totalCharacters: number;
+    pages: number;
+    perPage: number;
+}
+
 const Characters: React.FC = () => {
+    const [characters, setCharacters] = useState([]);
+    const [pagination, setPagination] = useState<Pagination>({
+        current: 0,
+        totalCharacters: 0,
+        pages: 0,
+        perPage: 20
+    });
+
+    useEffect(() => {
+        axios.get(`/character?page=${pagination.current}`).then((res) => {
+            const { info, results } = res.data;
+            setCharacters(results);
+            setPagination((oldPagination) => ({
+                ...oldPagination,
+                totalCharacters: info.count,
+                pages: info.pages
+            }));
+        });
+    }, []);
+
     return (
         <div className={styles.characters}>
             <Row gutter={32}>
-                <Col xxl={12} xl={12} lg={12} md={12} sm={24}>
-                    <Character
-                        title="Arcade Alien"
-                        avatar="https://rickandmortyapi.com/api/character/avatar/23.jpeg"
-                        status="unknown"
-                        species="Alien"
-                        location="Earth (Replacement Dimension)"
-                        seen="The Wedding Squanchers"
-                    />
-                </Col>
-                <Col xxl={12} xl={12} lg={12} md={12} sm={24}>
-                    <Character
-                        title="Arcade Alien"
-                        avatar="https://rickandmortyapi.com/api/character/avatar/23.jpeg"
-                        status="Dead"
-                        species="Alien"
-                        location="Earth (Replacement Dimension)"
-                        seen="The Wedding Squanchers"
-                    />
-                </Col>
+                {characters.map((character: any) => (
+                    <Col xxl={12} xl={12} lg={12} md={24} sm={24} key={character.id}>
+                        <Character
+                            title={character.name}
+                            avatar={character.image}
+                            status={character.status}
+                            species={character.species}
+                            location={character.location.name}
+                            seen="The Wedding Squanchers"
+                        />
+                    </Col>
+                ))}
             </Row>
         </div>
     );
